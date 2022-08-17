e70730988a4d173a50343ee9ae5bc4aacfb5f9b9
pawarajit98 <pawarajit98@gmail.com>
2022-08-17 22:08:10 +0530

    First Git Hub Added

---
+++ src/com/demo/Test.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+public class Test {
+	
+public static void main(String[] args) {
+		
+		String str ="Java";
+		
+		if(str=="java") {
+			System.out.println("yes we can Proceed");
+		}
+		
+		else {
+			System.out.println("We cant proceed");
+		}
+	}
+
+}
+
+
+
+
+
