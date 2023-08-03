73c3c5de8e991ec9d03302f54eb66ef20e4f9ade
Ajit <programming>
2023-08-03 10:44:30 +0530

    Initialized Commit

---
+++ Test.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		System.out.println(i);
+		
+		int a = 20; int b = 30;
+		System.out.println(a+b);
+		
+		int j = 10;
+		for (int k=20;k>j;k++) {
+			
+			System.out.println(k);
+			break;
+		}
+		
+		
+	}
+
+}
